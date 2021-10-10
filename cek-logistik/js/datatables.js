4437e14c3b3d99b37b4121878abdff2288b15d18
Rizky Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2021-10-10 17:00:37 +0700

    improve code styles

--- cek-logistik/js/datatables.js
+++ cek-logistik/js/datatables.js
@@ -1,78 +1,77 @@
 //script datatables admin/tabel-cek.php
-$(document).ready(function() {
-    var table = $('#tabel-print').DataTable( {
-        lengthChange: false,
-        buttons: [ 
-            {
-                extend: 'print',
-                exportOptions: {
-                    columns: [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ]
-                }
-            },
-            {
-                extend: 'excel',
-                exportOptions: {
-                    columns: [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ]
-                },
-                // customize: function ( xlsx ) {
-                //     var sheet = xlsx.xl.worksheets['sheet1.xml'];
-                //     $( 'row c', sheet ).attr( 's', '25' );
-                //     $( 'row:first c', sheet ).attr( 's', '51' );
-                // }
-            },
-            {
-                extend: 'pdf',
-                exportOptions: {
-                    columns: [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ]
-                }
-            },
-        ],
-        "order": [[ 6, "desc" ]]
-    } );
+$(document).ready(function () {
+  var table = $("#tabel-print").DataTable({
+    lengthChange: false,
+    buttons: [
+      {
+        extend: "print",
+        exportOptions: {
+          columns: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        },
+      },
+      {
+        extend: "excel",
+        exportOptions: {
+          columns: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        },
+        // customize: function ( xlsx ) {
+        //     var sheet = xlsx.xl.worksheets['sheet1.xml'];
+        //     $( 'row c', sheet ).attr( 's', '25' );
+        //     $( 'row:first c', sheet ).attr( 's', '51' );
+        // }
+      },
+      {
+        extend: "pdf",
+        exportOptions: {
+          columns: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        },
+      },
+    ],
+    order: [[6, "desc"]],
+  });
 
-    table.buttons().container().appendTo( '#tabel-print_wrapper .col-md-6:eq(0)' );
-} );
+  table.buttons().container().appendTo("#tabel-print_wrapper .col-md-6:eq(0)");
+});
 
 //script datatables admin/tabel-barang.php
-$(document).ready(function() {
-    var table = $('#tabel-barang').DataTable( {
-        lengthChange: false,
-        buttons: [ 
-            {
-                extend: 'print',
-                exportOptions: {
-                    columns: [ 0, 1, 2, 3, 4, 5 ]
-                }
-            },
-            {
-                extend: 'excel',
-                exportOptions: {
-                    columns: [ 0, 1, 2, 3, 4, 5 ]
-                },
-                customize: function ( xlsx ) {
-                    var sheet = xlsx.xl.worksheets['sheet1.xml'];
-                    $( 'row c', sheet ).attr( 's', '25' );
-                    $( 'row:first c', sheet ).attr( 's', '51' );
-                }
-            },
-            {
-                extend: 'pdf',
-                exportOptions: {
-                    columns: [ 0, 1, 2, 3, 4, 5 ]
-                }
-            },
-        ]
-    } );
-  
-    table.buttons().container()
-        .appendTo( '#tabel-barang_wrapper .col-md-6:eq(0)' );
-  } );
+$(document).ready(function () {
+  var table = $("#tabel-barang").DataTable({
+    lengthChange: false,
+    buttons: [
+      {
+        extend: "print",
+        exportOptions: {
+          columns: [0, 1, 2, 3, 4, 5],
+        },
+      },
+      {
+        extend: "excel",
+        exportOptions: {
+          columns: [0, 1, 2, 3, 4, 5],
+        },
+        customize: function (xlsx) {
+          var sheet = xlsx.xl.worksheets["sheet1.xml"];
+          $("row c", sheet).attr("s", "25");
+          $("row:first c", sheet).attr("s", "51");
+        },
+      },
+      {
+        extend: "pdf",
+        exportOptions: {
+          columns: [0, 1, 2, 3, 4, 5],
+        },
+      },
+    ],
+  });
+
+  table.buttons().container().appendTo("#tabel-barang_wrapper .col-md-6:eq(0)");
+});
 
 // script datatables tabel-cek.php
-$(document).ready(function() {
-    var table = $('#tabel-cek').DataTable( {
-        lengthChange: true,
-        "order": [[6, "desc"]]
-    });
-    table.buttons().container().appendTo('#tabel-cek_wrapper .col-md-6:eq(0)');
+$(document).ready(function () {
+  var table = $("#tabel-cek").DataTable({
+    lengthChange: true,
+    order: [[6, "desc"]],
+  });
+  table.buttons().container().appendTo("#tabel-cek_wrapper .col-md-6:eq(0)");
 });
