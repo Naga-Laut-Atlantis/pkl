928da8c92e9c436e8b6fedcf05869eb23b1c0427
Fadhillah Ahya Ramadhan <76592524+fahyar@users.noreply.github.com>
2021-10-10 16:32:44 +0700

    fix modal-admin

--- cek-logistik/js/modal-admin.js
+++ cek-logistik/js/modal-admin.js
@@ -1,22 +1,22 @@
 // script untuk modal hapus cek
 $('#hapusModal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget); // Button that triggered the modal
-  var recipient = button.data('whatever'); // Extract info from data-* attributes
-  var recipient1 = button.data('whatever1'); // Extract info from data-* attributes
+  var recipient = button.data("whatever"); // Extract info from data-* attributes
+  var recipient1 = button.data("whatever1"); // Extract info from data-* attributes
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
+  var modal = $(this);
   var url = "delete-cek.php?kode_brg="+recipient+"&tgl="+recipient1;
   modal.find('a').attr("href", url);
 });
 
 // script untuk modal hapus barang
-$('#hapusModalBarang').on('show.bs.modal', function (event) {
+$("#hapusModalBarang").on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget); // Button that triggered the modal
-  var recipient = button.data('whatever'); // Extract info from data-* attributes
+  var recipient = button.data("whatever"); // Extract info from data-* attributes
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
+  var modal = $(this);
   var url = "delete-barang.php?kode_brg="+recipient;
   modal.find('a').attr("href", url);
 });
@@ -60,7 +60,7 @@ function getBarang(kode) {
 $(document).ready(function () {
   window.setTimeout(function () {
       $(".alert").fadeTo(500, 0).slideUp(500, function(){
-          $(this).alert('close');
+          $(this).alert("close");
       });
   }, 3500);
 });
